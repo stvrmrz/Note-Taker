1ddc4a80124b6d02ad60521b9622771b09d83f72
Steve <stvchrm@steves-mbp.lan>
2024-07-07 13:04:10 -0600

    adding route to save note

--- server.js
+++ server.js
@@ -16,3 +16,18 @@ app.get('/api/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+
+// Route to save a new note
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        newNote.id = notes.length ? notes[notes.length - 1].id + 1 : 1;  // Assign an ID
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
